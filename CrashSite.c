2f705b2d2f4291c0a15b3ec56544233bcf2b69df
MergHQ <hugo.holm@live.com>
2018-12-27 00:12:55 +0200

    Fix crashsite and loot not spawning, add more crashsite loot and positions

--- CrashSite.c
+++ CrashSite.c
@@ -4,24 +4,45 @@
 
 class CrashSite {
 
-  ref array<vector> m_crashSites;
+  protected int MAIN_ITEM_AMOUNT = 10;
+  protected int MISC_ITEM_AMOUNT = 15;
+
+  ref array<vector> m_crashSites = {
+    "12856.1 6.59166 9064.85",
+    "12524.4 11.7239 8949.71",
+    "12370.2 21.9692 9355.82",
+    "5738.98 316.979 8362.11",
+    "5523.39 323.301 8578.56",
+    "10401.9 6.00843 2429.99",
+    "12392.9 150.4 12766.4",
+    "4468.57 342.419 10425.4",
+  };
 
   protected ref TStringArray m_wrecks = {
     "Wreck_Mi8",
     "Wreck_UH1Y",
   };
 
-  protected ref TStringArray m_weapons = {
+  protected ref TStringArray m_mainItems = {
     "AKM", 
     "M4A1", 
-    "izh18", 
-    "mp5k", 
-    "ump45", 
-    "svd", 
+    "Izh18", 
+    "MP5K", 
+    "UMP45", 
     "mosin9130",
     "mosin9130_black",
 		"mosin9130_green",
     "mosin9130_camo",
+    "AliceBag_Camo",
+    "AliceBag_Green",
+    "GhillieBushrag_Mossy",
+    "GhillieSuit_Mossy",
+    "M65Jacket_Black",
+    "USMCJacket_Woodland",
+    "USMCPants_Woodland",
+    "CombatKnife",
+    "SVD",
+    "FNX45",
   };
 
   protected ref TStringArray m_misc = {
@@ -43,14 +64,19 @@ class CrashSite {
     "mag_cmag_40rnd", 
     "mag_cmag_40rnd_black", 
     "mag_cmag_40rnd_green",
+    "MP5_PlasticHndgrd",
+    "PUScopeOptic",
+    "PSO1Optic",
+    "M68Optic",
+    "AK_Woodbttstck",
+    "M4_PlasticHndgrd",
+    "M4_MPBttstck",
+    "M4_T3NRDSOptic",
+    "M4_Suppressor",
   };
 
   int framesPassed = 0;
   bool shouldSpawn = true;
-
-  void CrashSite() {
-    m_crashSites = new array<vector>;
-  }
 
   void PollCrashSite() {
     if (framesPassed < (600 / 0.1)) {
@@ -64,42 +90,28 @@ class CrashSite {
     }
   }
 
-  void SpawnCrashSites(int num) {
-    for (int i = 0; i < num; i++) {
-      vector pos = SnapToGround("9534.84 310.333 8960.49");
-      GetGame().CreateObject(m_wrecks[1], pos, false, true );
-      m_crashSites.Insert(pos);
+  void SpawnCrashSites() {
+    for (int i = 0; i < m_crashSites.Count(); i++) {
+      vector pos = m_crashSites.Get(i);
+      GetGame().CreateObject(m_wrecks[1], pos, false, true);
+      Print("Spawning crashsite at " + pos);
     }
   }
 
   void SpawnLootOnCrashSites() {
     for (int i = 0; i < m_crashSites.Count(); i++) {
       vector pos = m_crashSites.Get(i);
-      for (int j = 0; j < 6; j++) {
-        float randX = Math.RandomFloat(-1, 1);
-        float randZ = Math.RandomFloat(-1, 1);
-        Print("Spawning weapon randX " + randX);
-        Print("Spawning weapon randZ " + randZ);
-        GetGame().CreateObject(m_weapons.GetRandomElement(), pos + Vector(4.0 * Math.Sin(randX), -3.5, 4.0 + (8.0 * Math.Cos(randZ))), false, true);
+      Print("Spawning loot for crashsite at " + pos);
+      for (int j = 0; j < MAIN_ITEM_AMOUNT; j++) {
+        float randX = Math.RandomFloat(0, 360);
+        float randZ = Math.RandomFloat(0, 360);
+        GetGame().CreateObject(m_mainItems.GetRandomElement(), pos + Vector(4.0 * Math.Sin(randX), -3.5, (8.0 * Math.Cos(randZ))), false, true);
       }
-      for (int k = 0; k < 10; k++) {
-        randX = Math.RandomFloat(-1, 1);
-        randZ = Math.RandomFloat(-1, 1);
-        Print("Spawning misc randX " + randX);
-        Print("Spawning misc randZ " + randZ);
-        GetGame().CreateObject(m_misc.GetRandomElement(), pos + Vector(4.0 * Math.Sin(randX), -3.5, 4.0 + (8.0 * Math.Cos(randZ))), false, true);
+      for (int k = 0; k < MISC_ITEM_AMOUNT; k++) {
+        randX = Math.RandomFloat(0, 360);
+        randZ = Math.RandomFloat(0, 360);
+        GetGame().CreateObject(m_misc.GetRandomElement(), pos + Vector(4.0 * Math.Sin(randX), -3.5, (8.0 * Math.Cos(randZ))), false, true);
       }
     }
-  }
-
-  vector SnapToGround(vector pos)
-  {
-      float pos_x = pos[0];
-      float pos_z = pos[2];
-      float pos_y = GetGame().SurfaceY( pos_x, pos_z );
-      vector tmp_pos = Vector( pos_x, pos_y, pos_z );
-      tmp_pos[1] = tmp_pos[1] + pos[1];
-
-      return tmp_pos;
   }
 };
